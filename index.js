602b79dd56a3a7a6135cb11921aca77907b4e6fa
ned <nedschultz@outlook.com>
2023-01-12 14:08:45 +1000

    commit tree 0604fcd6a7eceff42defdc0f17fd6f933a6257ef

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
+document.addEventListener('DOMContentLoaded', function () {
+  fetchBooks();
+});
+
 function fetchBooks() {
-  // To pass the tests, don't forget to return your fetch!
-  
+  return fetch("https://anapioficeandfire.com/api/books")
+    .then((resp) => resp.json())
+    .then((data) => renderBooks(data));
+
 }
 
 function renderBooks(books) {
@@ -11,7 +17,3 @@ function renderBooks(books) {
     main.appendChild(h2);
   });
 }
-
-document.addEventListener('DOMContentLoaded', function() {
-  fetchBooks();
-});
